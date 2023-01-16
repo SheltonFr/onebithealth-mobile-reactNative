e1cdf41f3a61fb16a087faefa60c16e7ff2c90a1
Shelton Francisco <sheltonfjs@gmail.com>
2023-01-16 19:26:31 +0200

    feat: share api

--- src/components/Form/ResultImc/index.jsx
+++ src/components/Form/ResultImc/index.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Share, TouchableOpacity, Alert } from 'react-native'
 import styles from './style'
 
 export default function ResultImc({ messageResultImc, resultImc }) {
+
+    const onShare = async () => {
+        Alert.alert('Share Alert', 'Do you wanna share your Imc?',
+            // alert buttons
+            [
+                {
+                    text: 'Cancel',
+                    style: 'cancel',
+                },
+
+                {
+                    text: 'Share',
+                    onPress: async () => {
+                        await Share.share({
+                            message: 'Meu Imc hoje é: ' + resultImc
+                        })
+                    }
+                }
+            ],
+            // options
+            {
+                cancelable: true, // close alert modal by touching anywhere on the screen
+            }
+        )
+    }
+
     return (
         <View>
+            <View style={styles.shareButtomContainer}>
+                {resultImc &&
+                    <TouchableOpacity
+                        onPress={onShare}
+                        style={styles.shareButtom}
+                    >
+                        <Text style={styles.buttomText}>Share</Text>
+                    </TouchableOpacity>
+                }
+            </View>
             <Text style={styles.message}>{messageResultImc}</Text>
             <Text style={styles.result}>{resultImc}</Text>
         </View>
--- src/components/Form/ResultImc/style.js
+++ src/components/Form/ResultImc/style.js
@@ -14,6 +14,28 @@ const styles = StyleSheet.create({
     fontSize: 50,
     textAlign: "center",
   },
+
+  shareButtomContainer: {
+    width: "100%",
+    alignContent: "center",
+    marginBottom: 10,
+  },
+
+  shareButtom: {
+    backgroundColor: "#1877f2",
+    borderRadius: 50,
+    alignContent: "center",
+    left: 10,
+  },
+
+  buttomText: {
+    textAlign: "center",
+    color: "#fff",
+    fontWeight: "bold",
+    paddingHorizontal: 30,
+    paddingVertical: 6,
+    fontSize: 15,
+  },
 });
 
 export default styles;
